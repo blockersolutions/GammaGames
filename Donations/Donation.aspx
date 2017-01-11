86997c87c433d367d0bb72bc13d32b0fe5ed71d0
William Blocker <blockersolutions@gmail.com>
2017-01-10 20:08:20 -0500

    Uploading Website

---
+++ Donations/Donation.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Donation.aspx.cs" Inherits="Donation" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h2>Thank You <%: Context.User.Identity.GetUserName()  %> <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label> for your <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label> donation to GammaGames!</h2>
+    <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConflictDetection="CompareAllValues" ConnectionString="<%$ ConnectionStrings:DefaultConnection %>" InsertCommand="INSERT INTO [Donations] ([UserName], [Amount]) VALUES (@UserName, @Amount)" OldValuesParameterFormatString="original_{0}">
+
+
+
+        <InsertParameters>
+
+
+        </InsertParameters>
+
+
+
+    </asp:SqlDataSource>
+
+</asp:Content>
+
---
+++ Donations/Donation.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+
+public partial class Donation : System.Web.UI.Page
+{
+    public string authToken;
+    public string query;
+    public string strResponse;
+    public string amount;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+
+            //read in txn token from querystring
+            amount = Request.QueryString["amt"];
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            SqlDataSource2.InsertParameters.Add("UserName", "Name");
+            SqlDataSource2.InsertParameters.Add("Amount", amount);
+
+
+            SqlDataSource2.Insert();
+        }
+        else
+        {
+            Label1.Text = "Oooops, something went wrong...";
+
+        }
+    }
+}
+
